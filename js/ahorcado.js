62e9de370b1d0560bd51bfec8a70882bd54545d4
Asdel <edmiasdel@gmail.com>
2022-03-15 15:36:54 -0500

    subiendo proyecto

---
+++ js/ahorcado.js
@@ -0,0 +1,118 @@
+var boton = document.querySelector("#iniciar-juego");
+boton.addEventListener("click", iniciarJuego);
+
+var boton_insertar = document.querySelector("#nueva-palabra");
+boton_insertar.addEventListener("click", agregarPalabra);
+
+var array_incorrectas = [];
+var array_correctas= [];
+//var intentos= 0;
+var partida = true;
+
+function dibujarLetra (palabra, letra){
+	
+	if(!array_correctas.includes(letra)){
+
+		
+		
+		x= 412;
+		var compara = false;
+		for(var i = 0; i < palabra.length; i++){
+
+			if(letra == palabra[i]){
+				compara=true;
+				pincel.font = "30px Coming Soon, Arial";
+				pincel.fillText(palabra[i], x, 495);
+				array_correctas.push(letra);
+				
+			}
+			
+			x = x + 30;
+		}
+
+		if(!compara){
+			
+			
+			letraIncorrecta(letra,intentos);
+		}
+		
+		if(array_correctas.length == palabra.length) {
+
+			gano(palabra);
+			return partida=false;
+		}
+
+		x=412;
+
+	}
+
+	
+}
+
+
+function letraIncorrecta(letra,partida){
+	
+	
+	if(!array_incorrectas.includes(letra)){
+		
+		dibujarHorca();	
+		array_incorrectas.push(letra);
+		
+
+	if(array_incorrectas.length==9){
+
+		perdio(palabra);
+		return partida= false;
+	}else{
+
+		for(var i = 0; i < array_incorrectas.length; i++){
+		
+			x= 410 +(30*i);
+			pincel.font = "30px Coming Soon, Arial";
+			pincel.fillText(array_incorrectas[i], x, 250);
+
+		}
+		return;
+	}
+
+	}
+	
+	return;
+	//console.log(array_incorrectas.includes(letra));
+	
+}
+
+
+function agregarPalabra(){
+	var input = document.querySelector("#input-nueva-palabra");
+	var palabra_agregada = input.value.trim().toUpperCase();
+	var expresion = /[A-Z]/;
+	var expresion2 = /[0-9]/;
+	var comparacion = palabra_agregada.match(expresion);
+	var comparacion2 = palabra_agregada.match(expresion2);
+	
+	if ((comparacion == null) || (comparacion2 != null)) {
+
+		noAceptaPalabra();
+		return;
+						
+	}else{
+				
+		arrayPalabras.push(palabra_agregada);
+		
+		siAceptaPalabra();
+		
+		setTimeout(function(){
+    		limpiarSpan();
+		}, 3000);
+		input.value="";
+		return;
+	}
+	
+	
+}
+
+
+
+
+
---
+++ js/canvas.js
@@ -0,0 +1,169 @@
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+
+var arrayPalabras = ["GATO", "PERRO", "CASA", "TOMATE", "CARRO", "VENTANA", "CIUDAD"];
+var intentos= 0;
+
+
+function palabraSecreta(array){
+	var palabra_seleccionada = array[Math.floor(Math.random()*array.length)]
+	
+	return palabra_seleccionada;
+}
+
+
+var palabra = palabraSecreta(arrayPalabras);
+
+
+//FUNCION PARA INICIAR EL JUEGO
+function iniciarJuego(){
+	
+	pantalla.classList.remove("oculto");
+	//console.log(palabra);
+	dibujarBase(); //DIBIJAMOS LA BASE DDEL AHORCADO
+	guiones();  //DIBUJAMOS LOS GUIONES DE LAS LETRAS
+	
+
+	if((intentos== 9) || (!partida)){
+		
+		return;
+		
+	}else{
+
+		window.addEventListener("keyup", function(event){ //CAPTURAR TECLA	
+			var letra_tipeada = event.key.toUpperCase();
+			var expresion = /[A-Z]/;
+			var comparacion = letra_tipeada.match(expresion);
+			if((intentos== 9) || (!partida)){
+			
+				return;		
+			}
+			
+			if ((comparacion == null) || (letra_tipeada.length > 1)) {
+
+				alert("Tecla presionada no es valida, solo se permiten letras");
+				
+				
+			}else{
+				
+				dibujarLetra(palabra, letra_tipeada);
+			}		
+		
+
+		});
+	}
+	
+	
+
+}
+
+
+///FUNCION PARA DINUJAR GUIONES
+function guiones (){
+	var cantidad = palabra.length;
+	var x=400;
+	var y=500;		
+
+
+	for(var i=1; i<=cantidad; i++){
+
+		
+		
+
+
+		///DIBIJAMOS LAS LINEAS SEGUN LAS PALABRAS
+		pincel.beginPath();	
+		pincel.lineWidth = 2;	
+		pincel.moveTo((x+10), y);
+		pincel.lineTo((x+35),y);
+		pincel.stroke();
+		
+		x= x +30;
+	}
+		
+}
+
+
+///FUNCION PARA DINUJAR LA BASE DEL AHORCADO
+function dibujarBase(){
+
+		pincel.fillStyle= "black";
+		pincel.beginPath();
+		pincel.moveTo(70,470);
+		pincel.lineTo(130,500);
+		pincel.lineTo(10,500);
+		pincel.fill();
+}
+
+
+function dibujarHorca(){
+
+	intentos++;
+	pincel.beginPath();
+	pincel.lineWidth = 5;
+	
+	if(intentos == 1){
+			
+		pincel.moveTo(70,473);
+		pincel.lineTo(70,50);
+		pincel.stroke();
+		
+	}if(intentos == 2){
+			
+		pincel.moveTo(70,50);
+		pincel.lineTo(250,50);
+		pincel.stroke();
+		
+	}if(intentos == 3){
+			
+		pincel.moveTo(250,50);
+		pincel.lineTo(250,100);
+		pincel.stroke();
+		
+	}if(intentos == 4){
+			
+		pincel.arc(250,148,50,0,2*Math.PI); //círculo completo.
+		pincel.stroke();
+		
+	}if(intentos == 5){//TONCO
+			
+		pincel.moveTo(250,198);
+		pincel.lineTo(250,400);
+		pincel.stroke();
+		
+	}if(intentos == 6){ //PIERNA 1
+			
+		pincel.moveTo(250,400);
+		pincel.lineTo(200,480);
+		pincel.stroke();
+		
+	}if(intentos == 7){ //PIERNA 2
+			
+		pincel.moveTo(250,400);
+		pincel.lineTo(300,480);
+		pincel.stroke();
+		
+	}if(intentos == 8){ //BRAZO 1
+			
+		pincel.moveTo(250,300);
+		pincel.lineTo(180,245);
+		pincel.stroke();
+		
+	}if(intentos == 9){ //BRAZO 2
+			
+		pincel.moveTo(250,300);
+		pincel.lineTo(320,245);
+		pincel.stroke();
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
