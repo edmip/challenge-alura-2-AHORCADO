6181c6d94611b4494cf6059c22c8fb837a997041
edmip <edmiasdel@gmail.com>
2024-08-13 15:34:17 -0500

    prueba desde la pc

---
+++ js/alertas.js
@@ -0,0 +1,41 @@
+var mensaje = document.querySelector("#eti-span");
+
+function perdio(palabra){
+
+
+		pincel.fillStyle = "red";
+		pincel.font = "30px Coming Soon, Arial";
+		pincel.fillText("USTED HA PERDIDO ", 370, 320);
+		pincel.fillText(" EL JUEGO", 430, 350);
+		pincel.fillStyle = "black";
+		pincel.fillText("PALABRA: " + palabra, 370, 385);
+		return;
+}
+
+function gano(palabra){
+
+		pincel.fillStyle = "green";
+		pincel.font = "30px Coming Soon, Arial";
+		pincel.fillText("USTED HA GANADO..!! ", 370, 320);
+		pincel.fillText(" EL JUEGO", 430, 350);
+		pincel.fillStyle = "black";
+		pincel.fillText("PALABRA: " + palabra, 370, 385);
+		return;
+}
+
+function noAceptaPalabra(){
+	mensaje.classList.remove("aceptada");
+	mensaje.classList.add("no-aceptada");
+	mensaje.innerHTML="Palabra introdicida no es valida, solo se permiten letras mayusculas";
+}
+
+function siAceptaPalabra(){
+
+	mensaje.classList.add("aceptada");
+	mensaje.innerHTML="Palabra exitosamente introdicida a la lista";
+}
+
+function limpiarSpan(){
+	mensaje.innerHTML="";
+}
+
